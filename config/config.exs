8eb5d54ac5e1a8ec30f2502f4c46fb6b87bb2f8a
hmteam <null@null.com>
2025-05-15 10:40:31 -0400

    generate user auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :disposocial3, :scopes,
+  user: [
+    default: true,
+    module: Disposocial3.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Disposocial3.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :disposocial3,
   ecto_repos: [Disposocial3.Repo],
   generators: [timestamp_type: :utc_datetime]
